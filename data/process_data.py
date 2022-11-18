ef4dbac3edb406dba6309e7278296ebabe77159a
Author Name <jxu1888@gmail.com>
2022-11-18 02:49:43 -0600

    forgot to push this change

--- data/process_data.py
+++ data/process_data.py
@@ -8,7 +8,7 @@ def distance(lat1, long1, lat2, long2):
     point2 = (lat2, long2)
     return geopy.distance.distance(point1, point2).miles
 
-airports = pd.read_csv('data/airports.dat')
+airports = pd.read_csv('airports.dat')
 airports.drop(airports.columns[[2, 3, 4, 5, 8, 9, 10, 11, 12, 13]], axis=1, inplace=True) 
 airports.dropna(inplace=True) 
 airports = airports[
@@ -27,7 +27,7 @@ airport_information = {}
 for row in airports:
     airport_information[row[0]] = [row[1], row[2], row[3]]
 
-routes = pd.read_csv('data/routes.dat')
+routes = pd.read_csv('routes.dat')
 routes.drop(routes.columns[[0, 1, 2, 4, 6, 7, 8]], axis=1, inplace=True)
 routes.dropna(inplace=True)
 routes = routes[(routes.sourceairportid != '\\N') & (routes.destinationairportid != '\\N')]
@@ -41,11 +41,40 @@ for route in routes:
         route.append(distance(airport_information[sourceid][1], airport_information[sourceid][2], airport_information[destinationid][1], airport_information[destinationid][2]))
         routes_with_distance.append(route)
 
-with open('US_routes.dat', 'w', newline='') as f:
+with open('US_routes.dat', 'w', encoding='UTF-8', newline='') as f:
     writer = csv.writer(f)
     writer.writerows(routes_with_distance)
 
-with open('US_airports.dat', 'w', newline='') as f:
+with open('US_airports.dat', 'w', encoding='UTF-8', newline='') as f:
     writer = csv.writer(f)
     for key, value in airport_information.items():
         writer.writerow((key, value[0]))
+
+index_dict = {}
+index = 0
+for row in routes_with_distance:
+    if row[0] in index_dict.keys():
+        row[0] = index_dict[row[0]]
+    elif row[0] not in index_dict.keys():
+        index_dict[row[0]] = index
+        row[0] = index
+        index = index + 1
+    if row[1] in index_dict.keys():
+        row[1] = index_dict[row[1]]
+    elif row[1] not in index_dict.keys():
+        index_dict[row[1]] = index
+        row[1] = index
+        index = index + 1
+
+contiguous_airport_info = []
+for key, value in airport_information.items():
+    if str(key) in index_dict.keys():
+        contiguous_airport_info.append([index_dict[str(key)], value[0]])
+
+with open('US_routes_contiguous.dat', 'w', encoding='UTF-8', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerows(routes_with_distance)
+
+with open('US_airports_contiguous.dat', 'w', encoding='UTF-8', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerows(contiguous_airport_info)
