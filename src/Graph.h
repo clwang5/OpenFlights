7e9d50436470f2b54da392e2be53f1eda11eed7b
jasonxu1888 <jxu1888@gmail.com>
2022-12-13 03:17:35 +0000

    more comments

--- src/Graph.h
+++ src/Graph.h
@@ -14,18 +14,31 @@ using namespace std;
 
 class Graph {
     public:
-        Graph() = default;
-        Graph(unordered_map<int, vector<pair<int, double>>> m);
-        Graph(string routes, string airports);
+        Graph() = default; //default constructor
+
+        Graph(unordered_map<int, vector<pair<int, double>>> m); // constructor for testing purposes, nodeToAirportName map is not used
+
+        Graph(string routes, string airports); //constructor for OpenFlights/custom datsets
+
         // param aiports is only used for testing the algorithm, true if using airport data set, false if otherwise
-        double Dijkstra(int source, int dest, bool airports); //returns shortest distance
-        vector<int> getShortestPath();
-        vector<int> BFS(int source);
-        vector<int> Tarjan();
-        unordered_map<int, string> getNodeToAirportName();
+
+        double Dijkstra(int source, int dest, bool airports); // returns shortest distance, populates shortestPath vector
+
+        vector<int> BFS(int source); // returns traversal path
+
+        vector<int> Tarjan(); // returns vector of SCCs
+
+        unordered_map<int, string> getNodeToAirportName(); // getter for map of node to airport name
+
+        vector<int> getShortestPath(); // getter for shortest path
+
     private:
-        void TarjanHelper(int node, stack<int>& s, vector<bool>& onStack, vector<int>& disc, vector<int>& low, vector<int>& SCCs);
-        unordered_map<int, string> nodeToAirportName;
-        unordered_map<int, vector<pair<int, double>>> adjList;
-        vector<int> shortestPath;
+        void TarjanHelper(int node, stack<int>& s, vector<bool>& onStack, vector<int>& disc, vector<int>& low, vector<int>& SCCs); // helper function for Tarjan's algo
+
+        unordered_map<int, string> nodeToAirportName; // map node to airport name for outputting purposes
+
+        unordered_map<int, vector<pair<int, double>>> adjList; //adjacency list representation of data
+
+        vector<int> shortestPath; // populated during Dijkstra's algorithm since Dijkstra only returns the distance, but we also want to return path for output
+        
 };
